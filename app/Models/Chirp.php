7cdb8cfba4faa978b85188e1eb0d17c55455869c
nitisha2017098 <nitisingh8863@gmail.com>
2023-09-06 17:35:28 +0530

    build(added send notifications on mail property): chirper

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Events\ChirpCreated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -14,8 +15,13 @@ class Chirp extends Model
         'message',
     ];
 
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
+    ];
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 }
+                                            
